5e37c10123662e1ac844bd16824293da94419e1b
Teo <telmix_27@hotmail.com>
2013-12-07 16:12:45 +0000

    (removi os imports nao usados)

--- src/org/opencg/javac/facerecognition/cipher/TripleDESCipher.java
+++ src/org/opencg/javac/facerecognition/cipher/TripleDESCipher.java
@@ -2,15 +2,11 @@ package org.opencg.javac.facerecognition.cipher;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
